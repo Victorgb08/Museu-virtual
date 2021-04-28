2cedab9c70fa30c023fe58202f659f730043374c
Pedro Babosa_Rat <pedro.b_15@outlook.com>
2021-04-28 18:33:06 -0300

    Definição da img de fundo e os inputs

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,32 +1,28 @@
 import React from "react";
-import {Link, useHistory} from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 function Login(){
 
-    const history=useHistory();
+    return(
+        <div className="base">
+            <img src="https://images.pexels.com/photos/102127/pexels-photo-102127.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="Paleta"/>
+            <div className="conteiner">
+                <div>
+                    <Form className="inputs">
+                        <Form.Group controlId="Email">
+                            <Form.Control type="email" placeholder="E-mail" />
+                        </Form.Group> 
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <Link to="cadastro">Cadastro</Link>
-            <br/>
-            <Link to="footer" >Footer</Link>
-            <br/>
-            <Link to="home" >Home</Link>
-            <br/>
-            <Link to="maiscomentadas" >MaisComentadas</Link>
-            <br/>
-            <Link to="perfil" >Perfil</Link>
-            <br/>
-            <button
-                onClick={()=>{
-                    history.push("pinturas")
-                }}
-            >
-                Pinturas
-            </button>
-        </div>            
-        );
+                        <Form.Group controlId="Password">
+                           <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="dark">Login</Button>{' '}   
+                    </Form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
