b57f6211a8a7d9b59cca29bd4810c3c49c7dd5e0
Joao Vitor <joaovitorgatoaraujo@gmail.com>
2021-05-05 11:52:59 -0300

    Barra de Menu e bibliotecas
    O menu possui um avatar e nome

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,22 +7,35 @@ import Pinturas from "./pages/Pinturas";
 import Perfil from "./pages/Perfil";
 import MaisComentadas from "./pages/MaisComentadas";
 import Footer from "./pages/Footer";
+import Menu from "./pages/Menu";
 
 
 function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/home" component={Home}/>
+                <Route exact path="/login" component={Login}/>
                 <Route path="/cadastro" component={Cadastro}/>
                 <Route path="/pinturas" component={Pinturas}/>
                 <Route path="/perfil" component={Perfil}/>
                 <Route path="/maiscomentadas" component={MaisComentadas}/>
                 <Route path="/footer" component={Footer}/>
+                <Route path="/" component={UserMenu}/>
             </Switch>
         </BrowserRouter>
     )
 }
+
+function UserMenu(){
+    return(
+        <Menu>
+            <Switch>
+                <Route path="/home" component={Home}/>
+                <Route component={()=><Redirect to="/login" />}/>
+            </Switch>
+        </Menu>
+    );
+}
+
 
 export default Routes;
