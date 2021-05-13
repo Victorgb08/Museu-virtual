06a3bfc2878ea9974910c6ed3e4e017fc232f478
Pedro Babosa_Rat <pedro.b_15@outlook.com>
2021-05-13 13:02:07 -0300

    Home atualizada

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,8 +9,7 @@ const pinturas = [
   {
     id: 1,
     name: "Pintura1",
-    src:
-      "https://cdn.pixabay.com/photo/2020/02/05/13/02/waterfalls-4821153_960_720.jpg",
+    src: "https://cdn.pixabay.com/photo/2020/02/05/13/02/waterfalls-4821153_960_720.jpg",
     title: "Primeira Arte",
     description: "A arte é o compo em que a razão se confunde",
     alt: "Second slide",
@@ -18,8 +17,7 @@ const pinturas = [
   {
     id: 2,
     name: "Pintura2",
-    src:
-      "https://cdn.pixabay.com/photo/2013/07/19/00/18/splashing-165192_960_720.jpg",
+    src: "https://cdn.pixabay.com/photo/2013/07/19/00/18/splashing-165192_960_720.jpg",
     title: "Segunda Arte",
     description: "A arte é o compo em que a razão se confunde",
     alt: "Thirth slide",
@@ -27,8 +25,7 @@ const pinturas = [
   {
     id: 3,
     name: "Pintura3",
-    src:
-      "https://cdn.pixabay.com/photo/2013/02/06/15/07/fish-78577_960_720.jpg",
+    src: "https://cdn.pixabay.com/photo/2013/02/06/15/07/fish-78577_960_720.jpg",
     title: "Terceira Arte",
     description: "A arte é o compo em que a razão se confunde",
     alt: "Fourth slide",
@@ -41,36 +38,33 @@ function Home() {
   return (
     <>
       <div className="backgroundHome">
-        <div className="conteiner1">
-          <p className="style1">
-            <h3>O que te move a fazer arte?</h3>
-            <h3>
-              Compartilhe a sua arte no Arte One: onde sua arte tem valor.
-            </h3>
-          </p>
+          <div className="conteiner1">
+            <p className="style1">
+              <h3>O que te move a fazer arte?</h3>
+              <h3> Compartilhe a sua arte no Arte One: onde sua arte tem valor.</h3>
+            </p>
+          </div>
+
+          <div className="conteiner2">
+            <p className="style2">
+              <h3>As artes mais curtidas</h3>
+            </p>
+          </div>
+
+          <Carousel
+            className="carrosselArts"
+            autoPlay="true"
+            dynamicHeight="true"
+            onClickItem
+          >
+            {pinturas.map((artes) => (
+              <div className="artsMuseu">
+                <img name={artes.name} src={artes.src} alt={artes.alt} />
+              </div>
+            ))}
+          </Carousel>
+          <Footer />
         </div>
-
-        <div className="conteiner2">
-          <p className="style2">
-            <h3>As artes mais curtidas</h3>
-          </p>
-        </div>
-
-        <Carousel
-          className="carrosselArts"
-          autoPlay="true"
-          dynamicHeight="true"
-          onClickItem
-        >
-          {pinturas.map((artes) => (
-            <div className="artsMuseu">
-              <img name={artes.name} src={artes.src} alt={artes.alt} />
-            </div>
-          ))}
-        </Carousel>
-
-        <Footer />
-      </div>
     </>
   );
 }
